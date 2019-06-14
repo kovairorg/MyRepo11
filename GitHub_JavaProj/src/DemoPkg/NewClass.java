4761de63edddbd5387a73952e6e1d89aae04f26d
Gaurankv <Gaurankv@LAPTOP17.in.kovair.com>
2019-06-14 19:06:49 +0530

    GVGIT-1

--- GitHub_JavaProj/src/DemoPkg/NewClass.java
+++ GitHub_JavaProj/src/DemoPkg/NewClass.java
@@ -5,6 +5,7 @@ public class NewClass {
 	public NewClass() {
 		// TODO Auto-generated constructor stub
 		System.out.println("New Classsssssssssssssssss");
+		System.out.println("New Classssssssssssssssss222s");
 	}
 
 }
