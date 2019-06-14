047abca503ecc70538b7dfc06544f9242669e761
Gaurankv <Gaurankv@LAPTOP17.in.kovair.com>
2019-06-14 17:41:57 +0530

    New File TestCase_01 Added

---
+++ GitHub_JavaProj/src/DemoPkg/TestCase_01.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package DemoPkg;
+
+/**
+ * @author Gaurankv
+ *
+ */
+public class TestCase_01 {
+
+	/**
+	 * 
+	 */
+	public TestCase_01() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		System.out.println("Hello World at 5.41 PM");
+	}
+
+}
