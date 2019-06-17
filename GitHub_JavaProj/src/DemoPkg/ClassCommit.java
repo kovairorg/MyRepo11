8117e94166331da99b145d734ccd3bdc66c2ebb5
Gaurankv <Gaurankv@LAPTOP17.in.kovair.com>
2019-06-17 16:11:12 +0530

    QWE-01

---
+++ GitHub_JavaProj/src/DemoPkg/ClassCommit.java
@@ -0,0 +1,17 @@
+package DemoPkg;
+
+public class ClassCommit {
+
+	public ClassCommit() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i  = 0;
+		if( i == 0) {
+			System.out.println("ClassCommit");
+		}
+	}
+
+}
